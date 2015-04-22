f00d84fa541f7d9f6e40f22008239a2fb00a3b45
doronshai <doronshai@gmail.com>
2015-04-22 08:56:02 +0300

    Update bash_profile with git prompt and auto complition

--- Bash/bash_profile.sh
+++ Bash/bash_profile.sh
@@ -5,32 +5,40 @@ if [ -f ~/.bashrc ]; then
         . ~/.bashrc
 fi
 
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+fi
+
 # User specific environment and startup programs
 
 PATH=$PATH:$HOME/bin
 
 export PATH
 
-
-
-# .bashrc
-
 # User specific aliases and functions
 
 alias ll='ls -lahG'
+export PS1="\[\033]0;$MSYSTEM:${PWD//[^[:ascii:]]/?}\007\]\n\[\033[32m\]\u@\h \[\033[33m\]\w$(__git_ps33)\[\033[0m\]\n$ "
+source /etc/bash_completion.d/git
 
-export PS1='\[\e[1;31m\][\u@\h \W]\$\[\e[0m\] '
 export http_proxy=http://proxy:8080
 export https_proxy=http://proxy:8080
-export https_proxy=$http_proxy
 export ftp_proxy=$http_proxy
 export JAVA_HOME=/usr/java/jdk1.7.0_65
 export M2_HOME=/opt/apache-maven-3.0.5
 export M2=$M2_HOME/bin
 export PATH=$JAVA_HOME/bin:$M2:$PATH
-export DISPLAY=:11.0
 
-# Source global definitions
-if [ -f /etc/bashrc ]; then
-        . /etc/bashrc
-fi
+
+alias ll='ls -lahG'
+__git_ps33 () 
+{ 
+ local b="$(git symbolic-ref HEAD 2>/dev/null)";
+    if [ -n "$b" ]; then
+        printf " (%s)" "${b##refs/heads/}";
+    else
+        printf "";
+    fi
+}
+
