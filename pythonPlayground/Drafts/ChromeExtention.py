95b34e9502a1383c9e179d207a7373921bc568bf
doronshai <doronshai@gmail.com>
2015-06-16 15:18:48 +0300

    Fix as Python 2.7

--- pythonPlayground/Drafts/ChromeExtention.py
+++ pythonPlayground/Drafts/ChromeExtention.py
@@ -1,4 +1,4 @@
-import urllib.request
+import urllib
 import requests
 import json
 import sys
@@ -9,20 +9,21 @@ def printResults(data):
   theJSON = json.loads(data)
   print (theJSON["version"])
 
- #sdsdsd
   
 def buildURL(id):
     myURL='https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86&nacl_arch=x86-64&prod=chromecrx&prodchannel=stable&prodversion=40.0.2214.93&lang=en-US&x=id%3D' + str(id) + '%26v%3D0.0.0.0%26uc'
     return myURL
 
 def downloadFile(url):
-    urllib.request.urlretrieve(url,'test.crx')
+    urllib.urlretrieve(url,'test.crx')
 
 def extractFile(file):
     with zipfile.ZipFile(file, "r") as z:
         z.extractall("archive")
 
 def main():
+
+    #First argument can be pbjikboenpfhbbejgkoklgkhjpfogcam
     currentUrl = buildURL(sys.argv[1])
     downloadFile(currentUrl)
     extractFile('test.crx')
