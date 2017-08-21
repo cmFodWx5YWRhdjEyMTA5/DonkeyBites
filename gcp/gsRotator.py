0bdf0f24bcaf086eb50a2623b5c2c1c670011869
doronshai <doronshai@gmail.com>
2017-08-21 16:18:16 +0300

    Update gsRotator.py

--- gcp/gsRotator.py
+++ gcp/gsRotator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
+import boto
+from operator import itemgetter
+
 # Take the credentials of the storage and put them using gsutils config -a
 # If configuration exists it can be deleted by delete .boto file
 #    GOOGFKUTD7EGUKER3TIE
 #    Qv5b/outIULq4UAiAk4CFIWyugjT6qkLF7brhiM9
-
-import boto
-from operator import itemgetter
 
 ROTATOR_QUOTA=20
 BUCKET='ensilobackup'
@@ -14,6 +14,7 @@ GOOGLE_STORAGE = 'gs'
 LOCAL_FILE = 'file'
 
 d = {}
+print '---------------------------------------------------------------------------------'
 
 uri = boto.storage_uri(BUCKET, GOOGLE_STORAGE)
 for obj in uri.get_bucket():
@@ -26,11 +27,9 @@ for obj in uri.get_bucket():
   print ' MD5:\t%s' % id
   d[id] = date
 
-print '------------------------------'
+print '---------------------------------------------------------------------------------'
 
-print sorted(d.items(),key=itemgetter(1),reverse=False)[0]
-
-print len(d)
+print sorted(d.items(),key=itemgetter(1),reverse=True)[0]
 
 files_to_delete = 0
 
@@ -44,7 +43,7 @@ print "files to delete - " + str(files_to_delete)
 
 for x in range(0, files_to_delete):
   print "x is " + str(x)
-  print "Delete item " + str(sorted(d.items(),key=itemgetter(1),reverse=False)[x])
+  print "Will delete item " + str(sorted(d.items(),key=itemgetter(1),reverse=True)[x])
 
   uri = boto.storage_uri(BUCKET, GOOGLE_STORAGE)
   for obj in uri.get_bucket():
@@ -52,7 +51,7 @@ for x in range(0, files_to_delete):
     object_uri = boto.storage_uri(BUCKET + '/' + obj.name, GOOGLE_STORAGE)
     key = object_uri.get_key()
     id = key.etag.strip('"\'')  # Remove surrounding quotes
-
-    if id == sorted(d.items(),key=itemgetter(1),reverse=False)[x]:
+    current_id = sorted(d.items(),key=itemgetter(1),reverse=True)[x]
+    if id in current_id:
       print 'Deleting object: %s...' % obj.name
-      # obj.delete()
+      obj.delete()
