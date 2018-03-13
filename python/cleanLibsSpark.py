ea09d6ea7cce09a5f09b179467f7882043886d1f
doronshai <doronshai@gmail.com>
2018-03-13 16:26:08 +0200

    Create cleanLibsSpark.py

---
+++ python/cleanLibsSpark.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import urllib2, json, re, base64
+import artifactoryGenericCleaner
+
+# Artifactory url
+url = "http://233.27.444.555:8081/artifactory"
+# Artifactory repository
+repo = "sbt-local-snapshot"
+# Artifactory credentials encoded in base64
+base64credentials = base64.b64encode('%s:%s' % ("zzzzzz", "zzzzzzzz"))
+# Number of versions that should not be deleted
+num_versions = 3
+# The Full name of the SNAPSHOT VERSION
+main_snapshot_version = "1.0.0-SNAPSHOT"
+# Artifacts tuples in the format:
+# [("some.group", "artifact_name")]
+artifacts = [
+    ("com.taptica", "budget"),
+    ("com.taptica", "strategy"),
+    ("com.taptica", "key"),
+    ("com.taptica", "banner"),
+    ("com.taptica", "static"),
+    ("com.taptica", "strategybudget")
+]
+
+for group, artifact in artifacts:
+    artifactoryGenericCleaner.deleteArtifacts(url, base64credentials, repo, group, artifact, num_versions, main_snapshot_version)
