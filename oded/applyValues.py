6557e4f88977c5fd0f40274d9879e7f5e5d563f2
Doron Shai <doronshai@gmail.com>
2019-01-27 17:16:37 +0000

    fix data issue

--- oded/applyValues.py
+++ oded/applyValues.py
@@ -7,16 +7,16 @@ import urllib.parse
 
 
 
-class ContactDetails():
+class Locations():
     def __init__(self, filename):
         with open(filename, "r") as f_input:
             csv_input = csv.reader(f_input)
             self.details = list(csv_input)
 
-    def get_col_row(self, col, row):
+    def get_col_row(self, row, col):
         return self.details[row-1][col-1]
 
-data = ContactDetails("points.csv")
+data = Locations("points.csv")
 
 directory="requests_with_applied_values"
 if os.path.exists(directory):
@@ -30,12 +30,14 @@ if os.path.exists(directory_json):
 os.makedirs(directory_json)
 
 
-for i in range(1, 2): #299
-    current_lat = data.get_col_row(2, i+1)
-    current_lon = data.get_col_row(3, i+1)
+for i in range(1, 299, 1): #299
+    current = i + 1
+    print (current)
+    current_lat = data.get_col_row(current,2)
+    current_lon = data.get_col_row(current,3)
     print("Row {row} current_lat is {lat}, current_lon is {lon}".format(row=i, lat=current_lat, lon=current_lon))
     
-    old_name = "requests.txt.orig.templated.short"
+    old_name = "requests.txt.orig.templated"
     new_name =  directory + "/requests_applied_" + str(i) + ".txt"
     shutil.copy(old_name, new_name)
     for line in fileinput.input([new_name], inplace=True):
@@ -50,11 +52,12 @@ for i in range(1, 2): #299
             date = par['start_datetime'][0]
             date = date[:-9]
             print ("Date is " + date)
+            print ("Row is " + str(i) )
             json_file_name = directory_json + "/" + str(i) + "_" + date + ".json"
             # Download the file from `url` and save it locally under `file_name`:
             try:
                 with urllib.request.urlopen(url) as response, open(json_file_name, 'wb') as out_file:
-                    data = response.read() # a `bytes` object
-                    out_file.write(data)
+                    datafile = response.read() # a `bytes` object
+                    out_file.write(datafile)
             except urllib.error.HTTPError as e:
                 print("Error: ", e)
