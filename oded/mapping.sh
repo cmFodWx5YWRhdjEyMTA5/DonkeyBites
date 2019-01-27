30e95adfd9bb48c22b703fee930bf43ce1953f46
Doron Shai <doronshai@gmail.com>
2019-01-27 12:32:14 +0000

    changes

--- oded/mapping.sh
+++ oded/mapping.sh
@@ -1,26 +1,35 @@
-file="downloaded_jsons_before_filter/1_2019-01-14.json"
+#!/bin/bash
+#file="downloaded_jsons_before_filter/1_2019-01-14.json"
 
 declare -a pollutants=("co" "no2" "o3" "pm10" "pm25" "so2")
+
+LIST_OF_FILES=$(ls -lat downloaded_jsons_before_filter | awk '{ print $9}')
 
 rm -rf final_csv_files
 mkdir final_csv_files
 
-less $file | jq  .data[0].datetime
-less $file | jq  .data[0].indexes.baqi.display_name
+#less $file | jq  .data[0].indexes.baqi.display_name
 
 
 echo "hour,co,no2,o3,pm10,pm25,so2" > final_csv_files/1_2019-01-14.csv
 
-for hour in {0..23}
+for file in downloaded_jsons_before_filter/*
 do
-  LINE="$hour,"
-  for pollutant in "${pollutants[@]}"
+  filename=$(basename -- "$file")
+  echo 111 ----  $filename
+  filename="${filename%.*}"
+  echo 222 ----  $filename 
+  echo "File name is " $file "will be filtered and converted to" $filename ".csv"
+  for hour in {0..23}
   do
-      LINE="$LINE$(less $file | jq  .data[$hour].pollutants.$pollutant.concentration.value |  tr -d '"'),"
+    LINE="$hour,"
+    for pollutant in "${pollutants[@]}"
+    do
+        LINE="$LINE$(less $file | jq  .data[$hour].pollutants.$pollutant.concentration.value |  tr -d '"'),"
+    done
+    echo $LINE >> final_csv_files/$filename.csv
   done
-echo $LINE >> final_csv_files/1_2019-01-14.csv
 done
-
 
 
 
