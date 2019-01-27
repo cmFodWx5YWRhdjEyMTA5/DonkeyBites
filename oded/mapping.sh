f34bb85b240aac5f6322238ffa74b4cfc4313911
Doron Shai <doron.s@taptica.com>
2019-01-27 23:18:24 +0200

    progress

--- oded/mapping.sh
+++ oded/mapping.sh
@@ -11,21 +11,22 @@ mkdir final_csv_files
 #less $file | jq  .data[0].indexes.baqi.display_name
 
 
-echo "hour,co,no2,o3,pm10,pm25,so2" > final_csv_files/1_2019-01-14.csv
 
 for file in downloaded_jsons_before_filter/*
 do
   filename=$(basename -- "$file")
-  echo 111 ----  $filename
   filename="${filename%.*}"
-  echo 222 ----  $filename 
   echo "File name is " $file "will be filtered and converted to" $filename ".csv"
+  echo "HOUR,BreezoMeter AQI,CO,NO2,O3,PM10,PM25,SO2" > final_csv_files/$filename.csv
+
   for hour in {0..23}
   do
     LINE="$hour,"
+    LINE="$LINE$(less $file | jq .data[$hour].indexes.baqi.aqi_display | tr -d '"'),"
+
     for pollutant in "${pollutants[@]}"
     do
-        LINE="$LINE$(less $file | jq  .data[$hour].pollutants.$pollutant.concentration.value |  tr -d '"'),"
+        LINE="$LINE$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"'),"
     done
     echo $LINE >> final_csv_files/$filename.csv
   done
