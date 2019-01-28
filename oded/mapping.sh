a64d10ddbcd81ad977d0b01507fcceaf77a62002
Doron Shai <doron.s@taptica.com>
2019-01-28 10:09:16 +0200

    multiply

--- oded/mapping.sh
+++ oded/mapping.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #file="downloaded_jsons_before_filter/1_2019-01-14.json"
 
+apt install bc
+
 declare -a pollutants=("co" "no2" "o3" "pm10" "pm25" "so2")
 
 LIST_OF_FILES=$(ls -lat downloaded_jsons_before_filter | awk '{ print $9}')
@@ -26,7 +28,40 @@ do
 
     for pollutant in "${pollutants[@]}"
     do
-        LINE="$LINE$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"'),"
+      case $pollutant in
+      "co")
+          VALUE=$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"')
+          VALUE=$(echo "$VALUE * 1.145" | bc -l)
+          LINE="$LINE$VALUE,"
+          ;;
+      "no2")
+          VALUE=$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"')
+          VALUE=$(echo "$VALUE * 1.88" | bc -l)
+          LINE="$LINE$VALUE,"
+          ;;
+      "o3")
+          VALUE=$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"')
+          VALUE=$(echo "$VALUE * 2" | bc -l)
+          LINE="$LINE$VALUE,"
+          ;;
+      "pm10")
+          VALUE=$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"')
+          VALUE=$(echo "$VALUE * 1" | bc -l)
+          LINE="$LINE$VALUE,"
+          ;;
+      "pm25")
+          VALUE=$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"')
+          VALUE=$(echo "$VALUE * 1" | bc -l)
+          LINE="$LINE$VALUE,"
+          ;;
+      "so2")
+          VALUE=$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"')
+          VALUE=$(echo "$VALUE * 2.62" | bc -l)
+          LINE="$LINE$VALUE,"
+          ;;      
+      esac
+      shift        
+      LINE="$LINE$(less $file | jq .data[$hour].pollutants.$pollutant.concentration.value | tr -d '"'),"
     done
     echo $LINE >> final_csv_files/$filename.csv
   done
