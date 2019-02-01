c909d0290e052089cba45f427e181acd32ea718f
Doron Shai <doronshai@gmail.com>
2019-02-01 18:52:24 +0000

    Add instaCharts

---
+++ instaCharts/getCurrentAmounts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DT=$(date '+%Y%m%d%H%M%S');
+
+python3 /home/insta/InstaPy/get_amount_of_FOLLOWERS.py > /home/DonkeyBites/instaCharts/output.log 2>&1
+
+FOLLOWING=$(less /home/DonkeyBites/instaCharts/output.log  | grep FOLLOW | awk '{print $6}')
+FOLLOWERS=$(less /home/DonkeyBites/instaCharts/output.log  | grep FOLLOW | awk '{print $10}')
+
+echo "$DT -  $FOLLOWING" >> /home/DonkeyBites/instaCharts/FOLLOWING
+echo "$DT -  $FOLLOWERS" >> /home/DonkeyBites/instaCharts/FOLLOWERS
