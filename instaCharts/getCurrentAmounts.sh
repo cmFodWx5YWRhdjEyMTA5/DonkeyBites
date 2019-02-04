96e75cb9bb2804d974962005337d6d46a91d9bf0
Doron Shai <doronshai@gmail.com>
2019-02-04 09:11:22 +0000

    change

--- instaCharts/getCurrentAmounts.sh
+++ instaCharts/getCurrentAmounts.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-DT=$(date '+%Y%m%d%H%M%S');
+DT="$(date '+%Y-%m-%d %H:%M:%S')"
 
-python3 /home/insta/InstaPy/DORON/get_amount_of_FOLLOWERS.py > /home/DonkeyBites/instaCharts/output.log 2>&1
+python3 /home/InstaPy/get_amount_of_FOLLOWERS.py > /home/InstaPy/output.log 2>&1
 
-FOLLOWING=$(less /home/DonkeyBites/instaCharts/output.log  | grep FOLLOW | awk '{print $6}')
-FOLLOWERS=$(less /home/DonkeyBites/instaCharts/output.log  | grep FOLLOW | awk '{print $10}')
+FOLLOWING=$(less /home/InstaPy/output.log  | grep FOLLOW | awk '{print $6}')
+FOLLOWERS=$(less /home/InstaPy/output.log  | grep FOLLOW | awk '{print $10}')
 
-echo "$DT -  $FOLLOWING" >> /home/DonkeyBites/instaCharts/FOLLOWING
-echo "$DT -  $FOLLOWERS" >> /home/DonkeyBites/instaCharts/FOLLOWERS
+jq --arg DT "$DT" --arg FOLLOWERS "$FOLLOWERS" --arg FOLLOWING "$FOLLOWING" '.data.entries[.data.entries| length] |= . + {"Date": $DT, "Followers": $FOLLOWERS, "Following": $FOLLOWING }' followers.json > followers_temp.json
+
+rm -rf /home/InstaPy/followers.json
+mv /home/InstaPy/followers_temp.json /home/InstaPy/followers.json
+
+cp -f /home/InstaPy/followers.json /var/www/html/followers.json
