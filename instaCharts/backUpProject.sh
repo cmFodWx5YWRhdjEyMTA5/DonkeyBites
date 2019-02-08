f3453501aeb1662b0cb2ea880324f84c76595985
Doron Shai <doronshai@gmail.com>
2019-02-08 15:35:43 +0200

    Update Shell files

--- instaCharts/backUpProject.sh
+++ instaCharts/backUpProject.sh
@@ -1,3 +1,4 @@
+cd /home/DonkeyBites/instaCharts
 git pull
 git add *.json
 git commit -m"Update json"
@@ -5,5 +6,7 @@ git add *.log
 git commit -m"Update log"
 git add index.html
 git commit -m"Update index.html"
+git add *.sh
+git commit -m"Update Shell files"
 git push origin master
 
