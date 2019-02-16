0fd482069b3b60cc59d5f7da1723d54f400a5f7e
Doron Shai <doronshai@gmail.com>
2019-02-16 03:00:06 +0200

    Update Shell files

--- instaCharts/CurrentAmounts222222.sh
+++ instaCharts/CurrentAmounts222222.sh
@@ -5,5 +5,5 @@ set -x
 CLIENT=${1}
 SCRIPT_HOME="/home/DonkeyBites/instaCharts"
 
-
-cp -f ${SCRIPT_HOME}/${CLIENT}_followers.json /var/www/html/$CLINET/followers.json
+sed -e "s/__CLIENT__/${CLIENT}/" index.html > ${CLIENT}_index.html
+cp -f ${SCRIPT_HOME}/${CLIENT}_index.html /var/www/html/${CLIENT}/index.html
