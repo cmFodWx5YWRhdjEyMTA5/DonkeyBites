7a4b1c27575188d32f4fe1d9b6d249cb05e92be5
Doron Shai <doronshai@gmail.com>
2019-02-19 01:00:05 +0200

    Update Shell files

--- instaCharts/backUpProject.sh
+++ instaCharts/backUpProject.sh
@@ -1,5 +1,10 @@
 cd /home/DonkeyBites/instaCharts
+cp -f /home/InstaPy/app.py /home/DonkeyBites/instaCharts/Server/
+cp -rf /home/InstaPy/static/* /home/DonkeyBites/instaCharts/Server/static/
+cp -rf /home/InstaPy/templates/* /home/DonkeyBites/instaCharts/Server/templates/
 git pull
+git add Server/
+git commit -m"Update Server"
 git add *.json
 git commit -m"Update json"
 git add *.log
