931dc887ff2200bdb25621120a814f50309ef2a3
Doron Shai <doronshai@gmail.com>
2019-02-19 01:00:05 +0200

    Update Server

--- instaCharts/Server/app.py
+++ instaCharts/Server/app.py
@@ -1,11 +1,13 @@
-import os
+import os,requests
+from datetime import datetime
 from flask import Flask,render_template, request,json
 
 app = Flask(__name__)
 db_user="abc"
 db_password="123"
-@app.route('/')
-def hello():
+@app.route('/login')
+def login():
+    os.system('python3 /home/InstaPy/test.py')
     return 'Welcome to Python Flask!'
 
 @app.route('/signUp')
@@ -16,12 +18,15 @@ def signUp():
 def signUpUser():
     user =  request.form['username'];
     password = request.form['password'];
-    if user == db_user and password == db_password:
-        ###  put your code here ###
-        #return json.dumps({'status':'OK','user':user,'pass':password});
-        return "ok"
-    else:
-        #return json.dumps({'status':'NOK'});
-        return "nok"
+    follow()
+    return 'ok'
+
+def sendTelegram (message):
+    requests.get(
+        "https://api.telegram.org/bot730982013:AAHjGc7kf3csMAY680TZTn80WFnb-dcdzOs/sendMessage?chat_id=387986068&text=InstaPy {} @ {}".format(message, datetime.now().strftime("%H:%M:%S")))
+
+def follow():
+    sendTelegram("WORKING")
+
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=5000)
