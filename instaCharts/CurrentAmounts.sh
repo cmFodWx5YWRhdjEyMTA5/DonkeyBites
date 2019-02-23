8820a332d4598318cf07bbd689541c75605ba797
Doron Shai <doronshai@gmail.com>
2019-02-24 01:00:06 +0200

    Update Server

--- instaCharts/CurrentAmounts.sh
+++ instaCharts/CurrentAmounts.sh
@@ -2,7 +2,7 @@
 set -x
 
 DT="$(date +%s )"
-
+HUMANDATE="$(date +"%d-%m-%Y-%H-%M" )"
 CLIENT=${1}
 SCRIPT_HOME="/home/DonkeyBites/instaCharts"
 
@@ -15,7 +15,7 @@ python3 /home/InstaPy/get_amount_of_FOLLOWERS_${CLIENT}.py > ${SCRIPT_HOME}/${CL
 FOLLOWING=$(less ${SCRIPT_HOME}/${CLIENT}_output.log  | grep FOLLOW | awk '{print $6}')
 FOLLOWERS=$(less ${SCRIPT_HOME}/${CLIENT}_output.log  | grep FOLLOW | awk '{print $10}')
 
-jq --arg DT "$DT" --arg FOLLOWERS "$FOLLOWERS" --arg FOLLOWING "$FOLLOWING" '.data.entries[.data.entries| length] |= . + {"Date": $DT, "Followers": $FOLLOWERS, "Following": $FOLLOWING }' ${SCRIPT_HOME}/${CLIENT}_followers.json > ${SCRIPT_HOME}/${CLIENT}_followers_temp.json
+jq --arg DT "$DT" --arg HUMANDATE "$HUMANDATE" --arg FOLLOWERS "$FOLLOWERS" --arg FOLLOWING "$FOLLOWING" '.data.entries[.data.entries| length] |= . + {"Date": $DT, "HumanDate": $HUMANDATE, "Followers": $FOLLOWERS, "Following": $FOLLOWING }' ${SCRIPT_HOME}/${CLIENT}_followers.json > ${SCRIPT_HOME}/${CLIENT}_followers_temp.json
 
 rm -rf ${SCRIPT_HOME}/${CLIENT}_followers.json
 mv ${SCRIPT_HOME}/${CLIENT}_followers_temp.json ${SCRIPT_HOME}/${CLIENT}_followers.json
