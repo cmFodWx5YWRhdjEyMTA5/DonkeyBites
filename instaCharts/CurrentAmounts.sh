98cd3e3055144ec6ca4c4af691f8046a8998ec51
Doron Shai <doronshai@gmail.com>
2019-02-26 01:00:04 +0200

    Update Server

--- instaCharts/CurrentAmounts.sh
+++ instaCharts/CurrentAmounts.sh
@@ -6,9 +6,24 @@ HUMANDATE="$(date +"%d-%m-%Y-%H-%M" )"
 CLIENT=${1}
 SCRIPT_HOME="/home/DonkeyBites/instaCharts"
 
-if [ ! -f ${SCRIPT_HOME}/${CLIENT}_output.log ]; then
-  touch ${SCRIPT_HOME}/${CLIENT}_output.log
+if [ -f ${SCRIPT_HOME}/${CLIENT}_output.log ]; then
+  rm -rf ${SCRIPT_HOME}/${CLIENT}_output.log
 fi
+
+touch ${SCRIPT_HOME}/${CLIENT}_output.log
+
+#RUN_AGAIN='True'
+#while [ "$RUN_AGAIN" == "True" ]
+#do
+#  python3 /home/InstaPy/get_amount_of_FOLLOWERS_${CLIENT}.py > ${SCRIPT_HOME}/${CLIENT}_output.log 2>&1
+#  if grep -Fxq "Traceback" ${SCRIPT_HOME}/${CLIENT}_output.log
+#  then
+#    RUN_AGAIN=False
+#    echo "--- Check was succesful ----"
+#  else
+#    echo "==== Run again as the output is not valid"
+#  fi
+#done
 
 python3 /home/InstaPy/get_amount_of_FOLLOWERS_${CLIENT}.py > ${SCRIPT_HOME}/${CLIENT}_output.log 2>&1
 
---
+++ instaCharts/nightlyExecution.sh
@@ -0,0 +1,6 @@
+
+
+/home/DonkeyBites/instaCharts/CurrentAmounts.sh SHAKED
+* * * /home/DonkeyBites/instaCharts/CurrentAmounts.sh YOAV
+20 0 * * * /home/DonkeyBites/instaCharts/CurrentAmounts.sh doron
+30 0 * * * /home/DonkeyBites/instaCharts/CurrentAmounts.sh ofek
