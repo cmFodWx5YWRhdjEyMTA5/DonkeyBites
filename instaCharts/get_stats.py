7de3340abfc05c519b4715dffd3458fe75a8ec8e
Doron Shai <doron.s@taptica.com>
2019-03-08 20:46:23 +0200

    fix

--- instaCharts/get_stats.py
+++ instaCharts/get_stats.py
@@ -7,7 +7,7 @@ from InstagramAPI import InstagramAPI
 import sys
 import sqlite3, logging
 from database_engine import get_database
-from ettings import Settings
+from settings import Settings
 
 def save_account_progress(username, followers, following, posts, logger):
     """
